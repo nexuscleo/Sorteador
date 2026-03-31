d7c22fa102c27afe0caedba7a77969bf7b9e79d0
CleomarSilva <nexuscleo@gmail.com>
2026-03-31 00:29:58 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+const minInput = document.getElementById('min');
+const maxInput = document.getElementById('max');
+const button = document.querySelector('.button');
+const solutionDiv = document.querySelector('.solution');
+
+function generateNumber() {
+    const min = parseInt(minInput.value);
+    const max = parseInt(maxInput.value);
+    const solution = Math.floor(Math.random() * (max - min + 1)) + min;
+    solutionDiv.textContent = `Número sorteado: ${solution}`;
+    console.log(`Número sorteado: ${solution}`);
+}
+
+button.addEventListener('click', generateNumber);
+*/
+function generateNumber() {
+    
+    const minInput = Math.ceil(document.getElementById('min').value);
+    const maxInput = Math.floor(document.getElementById('max').value);
+    const solution = Math.floor(Math.random() * (maxInput - minInput)) + minInput;
+    alert(`Número sorteado: ${solution}`);
+}
