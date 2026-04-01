1ee7f9fb75755fb114c9a91c8e3b7be84b9a6612
CleomarSilva <nexuscleo@gmail.com>
2026-03-31 23:43:59 -0300

    adiciona botão para sortear número e exibe resultado na solução

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/*
-const minInput = document.getElementById('min');
-const maxInput = document.getElementById('max');
-const button = document.querySelector('.button');
-const solutionDiv = document.querySelector('.solution');
-
-function generateNumber() {
-    const min = parseInt(minInput.value);
-    const max = parseInt(maxInput.value);
-    const solution = Math.floor(Math.random() * (max - min + 1)) + min;
-    solutionDiv.textContent = `Número sorteado: ${solution}`;
-    console.log(`Número sorteado: ${solution}`);
-}
-
-button.addEventListener('click', generateNumber);
-*/
-
 const solutionDiv = document.querySelector('.solution');
 function generateNumber() {
     
